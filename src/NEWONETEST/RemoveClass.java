d5f18ff000c8a8dec32f9e2b15c7480e3edc9a59
Max <max.nazaruk@gmail.com>
2017-07-28 20:45:14 +0200

    comitment message

---
+++ src/NEWONETEST/RemoveClass.java
@@ -0,0 +1,10 @@
+package NEWONETEST;
+
+/**
+ * Created by Max on 7/28/2017.
+ */
+public class RemoveClass {
+    public static void main(String[] args) {
+        System.out.println("ASDASDaw");
+    }
+}
