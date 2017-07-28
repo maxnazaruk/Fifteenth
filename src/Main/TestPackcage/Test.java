ca109fb7decae23d4bde9c4157aeb3296f1cd091
Max <max.nazaruk@gmail.com>
2017-07-28 20:43:08 +0200

    comitment message

---
+++ src/Main/TestPackcage/Test.java
@@ -0,0 +1,10 @@
+package Main.TestPackcage;
+
+/**
+ * Created by Max on 7/28/2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("!@#!@#!@#");
+    }
+}
